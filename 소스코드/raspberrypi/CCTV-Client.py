fd716b75ce64c59136a8fc593e3b10de139abd91
yjj17 <94586664+yjj17@users.noreply.github.com>
2022-04-27 16:24:15 +0900

    Update CCTV-Client.py

--- 소스코드/raspberrypi/CCTV-Client.py
+++ 소스코드/raspberrypi/CCTV-Client.py
@@ -7,7 +7,7 @@ from _thread import *
 IMG_SIZE = 224
 enclosure_queue = Queue()
 
-#webcam으로부터 영상을 읽어서 queue에 넣는 쓰레드가 수행하는 함수
+# webcam으로부터 영상을 읽어서 queue에 넣는 쓰레드가 수행하는 함수
 def webcam_input(queue):
     capture = cv2.VideoCapture('./fire and smoke.mp4')
     speed=6
@@ -25,22 +25,22 @@ def webcam_input(queue):
         encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
         result, imgencode = cv2.imencode('.jpg', frame2, encode_param)
 
-        #영상을 문자열 자료형으로 변환 후 저장
+        # 영상을 문자열 자료형으로 변환 후 저장
         data = numpy.array(imgencode)
         stringData = data.tostring()
         queue.put(stringData)
 
-        #영상 출력
+        # 영상 출력
         cv2.imshow('image', frame)
         key = cv2.waitKey(1)
         if key == 27:
             return
 
-#queue에서 영상을 읽어서 socket을 이용해 서버로 전송하는 쓰레드가 수행하는 함수
+# queue에서 영상을 읽어서 socket을 이용해 서버로 전송하는 쓰레드가 수행하는 함수
 def webcam_transmit(client_socket, queue):
     while True:
         try:
-            #서버로 부터 메시지를 받을 때마다 영상을 한 장씩 전송
+            # 서버로부터 메시지를 받을 때마다 영상을 한 장씩 전송
             data = client_socket.recv(1024)
             if not data:
                 print('Disconnected')
@@ -58,16 +58,16 @@ def webcam_transmit(client_socket, queue):
 
     client_socket.close()
 
-#서버의 IP주소및 포트번호
-HOST = '192.168.219.106'
+# 서버의 IP주소 및 포트번호
+HOST = '192.168.0.12'
 PORT = 9999
-#소켓 생성및 연결
+# 소켓 생성 및 연결
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST,PORT))
 
 print('client start')
-#영상 읽기 쓰레드 생성
+# 영상 읽기 쓰레드 생성
 i = start_new_thread(webcam_input, (enclosure_queue,))
-#영상 전송 쓰레드 생성
+# 영상 전송 쓰레드 생성
 t = start_new_thread(webcam_transmit, (client_socket, enclosure_queue,))
 
